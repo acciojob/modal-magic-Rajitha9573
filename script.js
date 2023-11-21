da685a3f29d989a9571a2594b62792bd5cdebd2f
Rajitha9573 <141991018+Rajitha9573@users.noreply.github.com>
2023-11-21 18:09:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const btnElement =document.getElementById("openModel");
+const boxModel = document.getElementById("boxModel") ;
+btnElement.addEventListener("click", ()=>{
+	boxModel.style.display="flex";
+})
+const closeElement = document.getElementById("close") ;
+closeElement.addEventListener("click", ()=>{
+	boxModel.style.display="none";
+})
